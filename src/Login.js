8b52913e4eab6684f93d8fdbec99f2d296826966
david11yf29 <david11yf29@gmail.com>
2021-01-26 23:46:27 +0800

    updated

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Login.css";
 
+import { loginUrl } from "./spotify";
+
 const Login = () => {
   return (
     <div className="login">
@@ -9,7 +11,7 @@ const Login = () => {
         alt=""
       />
       {/* Login with spotify button  */}
-      <a>LOGIN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 };
--- src/spotify.js
+++ src/spotify.js
@@ -3,3 +3,15 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 
 const clientId = "9d7de7fea7ce4f719a271a57d0ba7916";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-playing",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_url=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
