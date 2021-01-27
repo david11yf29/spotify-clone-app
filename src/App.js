31abc483a2152f6f15a200bf117d0cfd2cf27bd3
david11yf29 <david11yf29@gmail.com>
2021-01-27 16:02:36 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 
+import SpotifyWebApi from "spotify-web-api-js";
+
 import { getTokenFromUrl } from "./spotify";
+
+const spotify = new SpotifyWebApi();
 
 const App = () => {
   const [token, setToken] = useState(null);
@@ -15,6 +19,12 @@ const App = () => {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
     }
 
     console.log("I have a token>>>", token);
