62a2f77cb41fcdf751a0420173373bc296543335
david11yf29 <david11yf29@gmail.com>
2021-01-27 21:17:22 +0800

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 const Sidebar = () => {
   return (
@@ -9,6 +10,9 @@ const Sidebar = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <SidebarOption option="Home" />
+      <SidebarOption option="Search" />
+      <SidebarOption option="Your library" />
     </div>
   );
 };
