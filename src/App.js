07b91ecc3c1b8b7931fafb3683bba5b270c58893
david11yf29 <david11yf29@gmail.com>
2021-01-27 15:23:55 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 
+import { getTokenFromUrl } from "./spotify";
+
 const App = () => {
+  const [token, setToken] = useState(null);
+
+  // Run code baased on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("I have a token>>>", token);
+  }, []);
+
   return (
-    // BEM
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 };
 
--- src/spotify.js
+++ src/spotify.js
@@ -12,6 +12,20 @@ const scopes = [
   "user-modify-playback-state",
 ];
 
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1) // # 這個符號我不要
+    .split("&") // 返回 array [token=ajdlfk, name=asdjflk]
+    .reduce((initial, item) => {
+      // [[token, ajdlfk], [name, asdjflk]]
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      // { token: decoce(ajdlfk), name: decode(sdjflk) }
+      return initial;
+    }, {});
+};
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
