76cf5d0476aa8afa8e3ce71fd4728c7b1d38286e
david11yf29 <david11yf29@gmail.com>
2021-01-27 20:29:37 +0800

    updated

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Player.css";
+import Sidebar from "./Sidebar";
 
-const Player = () => {
+const Player = ({ spotify }) => {
   return (
-    <div>
-      <h1>Welcome to Spotify</h1>
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        {/* Body */}
+      </div>
+
+      {/* Footer */}
     </div>
   );
 };
