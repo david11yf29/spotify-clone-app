6fe56fd84e4a24580eaf1e461de85c01019ed958
david11yf29 <david11yf29@gmail.com>
2021-01-27 20:54:29 +0800

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,11 @@ import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
     </div>
   );
 };
