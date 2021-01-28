1cc83a1d880e2f41f5c55525a5c57a9233b6bcac
david11yf29 <david11yf29@gmail.com>
2021-01-28 14:36:41 +0800

    updated

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,51 @@
 import React from "react";
 import "./Footer.css";
 
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__left">
-        <p>Album and song details</p>
+        <img
+          className="footer__albumLogo"
+          src="https://i.pinimg.com/originals/b0/a8/29/b0a829f1d7885ad81a9b16c41b97c4d2.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
       </div>
+
       <div className="footer__center">
-        <p>Player controls</p>
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
       </div>
+
       <div className="footer__right">
-        <p>Volume controls</p>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
